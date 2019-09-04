72f0c47db434212fcd7cb520b26a08c42e84e294
dani123yal <daniyal.safdar7@gmail.com>
2019-09-04 21:47:27 +0500

    login added

---
+++ HabeebFootwear/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using System.Web.Security;
+
+namespace HabeebFootwear.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form)
+        {
+            string uName = form["UserName"];
+            string pass = form["Password"];
+
+            try
+            {
+                if(WebSecurity.Login(uName, pass))
+                {
+                    ViewBag.error = Roles.GetRolesForUser(WebSecurity.CurrentUserName);
+                    return View();
+                }
+                else
+                {
+                    ViewBag.error = "failed";
+                }
+            }
+            catch 
+            {
+                
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
--- HabeebFootwear/Global.asax.cs
+++ HabeebFootwear/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using WebMatrix.WebData;
 
 namespace HabeebFootwear
 {
@@ -16,6 +18,27 @@ namespace HabeebFootwear
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            InitializeAuthenticationProcess();
+            //WebSecurity.CreateUserAndAccount("outlet_1","12345");
+            //Roles.AddUserToRole("outlet_1", "outlet");
+        }
+
+        private void InitializeAuthenticationProcess()
+        {
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("Habib", "Users", "UserId", "UserName", true);
+
+
+                if (!Roles.RoleExists("headOffice"))
+                {
+                    Roles.CreateRole("headOffice");
+                }
+                if (!Roles.RoleExists("outlet"))
+                {
+                    Roles.CreateRole("outlet");
+                }
+            }
         }
     }
 }
